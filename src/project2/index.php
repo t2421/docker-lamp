b36ee2d4cb4eef3ad3b7a453502b982064cada9c
Taiga Kiyotaki <taiga@prty.jp>
2019-04-02 17:07:05 +0900

    リファクタリング

--- src/project2/index.php
+++ src/project2/index.php
@@ -10,7 +10,7 @@ $options = array(PDO::MYSQL_ATTR_INIT_COMMAND=>"SET CHARACTER SET 'utf8'");
 $pdo = new PDO('mysql:host='.$db["host"].';port='.$db["port"].';dbname='.$db["name"], $db["id"], $db["pass"], $options);
 $stmt = $pdo->query("SELECT * FROM my_table");
 
-mb_send_mail("taiga@prty.jp","mailhogTest","本文");
+mb_send_mail("mailhog@example.com","mailhogTest","本文");
 
 ?>
 <?php foreach($stmt as $row): ?>
