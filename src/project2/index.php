f91328e92c14138e9bbc4705e651a5203a41fe7a
Taiga Kiyotaki <taiga@prty.jp>
2019-04-02 15:55:43 +0900

    mysqlサービスへの接続デモ追加

--- src/project2/index.php
+++ src/project2/index.php
@@ -1,4 +1,19 @@
 <?php
+$db = array(
+    'host' => "mysql", //docker-composeのサービス名でアクセス
+    'name' => "my_dev",
+    'port' => "3306", //ホストマシンからアクセスするわけではないので、3306
+    'id'   => "root",
+    'pass'   => "root"
+);
+$options = array(PDO::MYSQL_ATTR_INIT_COMMAND=>"SET CHARACTER SET 'utf8'");
+$pdo = new PDO('mysql:host='.$db["host"].';port='.$db["port"].';dbname='.$db["name"], $db["id"], $db["pass"], $options);
+$stmt = $pdo->query("SELECT * FROM my_table");
 
 ?>
+<?php foreach($stmt as $row): ?>
+<p>name <?= $row["name"] ?></p>
+<?php endforeach; ?>
+
 it works project2
+
