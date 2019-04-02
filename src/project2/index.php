aa589713b452c714c6771b42b0f59f1973f7cad8
Taiga Kiyotaki <taiga@prty.jp>
2019-04-02 17:00:47 +0900

    phpからmailhogのsmtp経由でメールを送信する機能実装

--- src/project2/index.php
+++ src/project2/index.php
@@ -10,6 +10,8 @@ $options = array(PDO::MYSQL_ATTR_INIT_COMMAND=>"SET CHARACTER SET 'utf8'");
 $pdo = new PDO('mysql:host='.$db["host"].';port='.$db["port"].';dbname='.$db["name"], $db["id"], $db["pass"], $options);
 $stmt = $pdo->query("SELECT * FROM my_table");
 
+mb_send_mail("taiga@prty.jp","mailhogTest","本文");
+
 ?>
 <?php foreach($stmt as $row): ?>
 <p>name <?= $row["name"] ?></p>
